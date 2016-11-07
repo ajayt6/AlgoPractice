db6a943e880e4ab885f5efb73313c92a4d5fd44e
Ajay Thomas <Ajay Thomas>
2016-11-06 20:26:38 -0600

    updated starAdventure in apples

--- DP_starAdventure.cpp
+++ DP_starAdventure.cpp
@@ -1,6 +1,4 @@
 /*
-Problem StarAdventure – SRM 208 Div 1:
-
 Given a matrix with M rows and N columns (N x M). In each cell there’s a number of apples.
 1)You start from the upper-left corner of the matrix. You can go down or right one cell. You need to arrive to the bottom-right corner.
 2)Then you need to go back to the upper-left cell by going each step one cell left or up. 
@@ -10,6 +8,8 @@ Find the maximum number of apples you can collect.
 When you pass through a cell – you collect all the apples left there.
 
 Restrictions: 1 < N, M <= 50 ; each cell contains between 0 and 1000 apples inclusive.
+
+Similar to Problem StarAdventure – SRM 208 Div 1: https://community.topcoder.com/stat?c=problem_statement&pm=2940&rd=5854
 */
 
 
@@ -26,22 +26,44 @@ Space Complexity = O(n^2)
 using namespace std;
 
 
-int apples[][3] = { { 1,2,3 },{ 4,6,5 },{ 8,2,1 } };
-int dupApples[3][3];
-pair<int,int> backtrackApples[3][3];
+//int apples[][3] = { { 1,2,3 },{ 4,6,5 },{ 8,2,1 } };
+int apples[][3] =
+{ {0,1,2}
+,{0,1,2}
+,{0,1,2}
+,{0,1,2}
+,{0,1,2}
+,{0,1,2}
+,{0,1,2} };
+int** dupApples = NULL;
+pair<int,int>** backtrackApples = NULL;
+
+int cols = sizeof(apples[0]) / sizeof(int);
+int rows = sizeof(apples) / sizeof(apples[0]);
+
 
 int maxApples()
 {
 	int totalApples = 0;
+
+	//Create dupApples and backtrackApples
+	dupApples = new int*[rows];
+	for (int i = 0; i < rows; i++)
+		dupApples[i] = new int[cols];
+
+	backtrackApples = new pair<int,int>*[rows];
+	for (int i = 0; i < rows; i++)
+		backtrackApples[i] = new pair<int, int>[cols];
+
 	for (int k = 0; k < 3; k++)
 	{
 
 		//Do the normal copying
 		if (k != 1)
 		{
-			for (int i = 0; i < 3; i++)
+			for (int i = 0; i < rows; i++)
 			{
-				for (int j = 0; j < 3; j++)
+				for (int j = 0; j < cols; j++)
 				{
 					dupApples[i][j] = apples[i][j];
 				}
@@ -50,11 +72,11 @@ int maxApples()
 		//Do reverse copying
 		else
 		{
-			for (int i = 0; i < 3; i++)
+			for (int i = 0; i <rows; i++)
 			{
-				for (int j = 0; j < 3; j++)
+				for (int j = 0; j < cols; j++)
 				{
-					dupApples[3-i-1][3-j-1] = apples[i][j];
+					dupApples[rows-i-1][cols-j-1] = apples[i][j];
 				}
 			}
 
@@ -62,9 +84,9 @@ int maxApples()
 
 
 		//The collection round
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < cols; j++)
 			{
 				if (i == 0 && j == 0)
 					backtrackApples[i][j] = make_pair(-1, -1);
@@ -86,12 +108,12 @@ int maxApples()
 				}
 			}
 		}
-		totalApples = totalApples + dupApples[2][2];
+		totalApples = totalApples + dupApples[rows-1][cols-1];
 
 		//Do the backtracking and clear apples
 		if (k != 1)
 		{
-				int i=2, j=2,newI,newJ;
+				int i=rows-1, j=cols-1,newI,newJ;
 				while(i>=0 && j>=0)
 				{
 					apples[i][j] = 0;
@@ -105,10 +127,10 @@ int maxApples()
 		//Do reverse backtracking
 		else
 		{
-				int i = 2, j = 2, newI, newJ;
+				int i = rows-1, j = cols-1, newI, newJ;
 				while (i >= 0 && j >= 0)
 				{
-					apples[3-i-1][3-j-1] = 0;
+					apples[rows-i-1][cols-j-1] = 0;
 					newI = backtrackApples[i][j].first;
 					newJ = backtrackApples[i][j].second;
 
@@ -119,9 +141,9 @@ int maxApples()
 
 		cout << endl << "The backtrack matrix after round " << k << " is: " << endl;
 
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < cols; j++)
 			{
 				cout << backtrackApples[i][j].first << " "<< backtrackApples[i][j].second<<"		";
 			}
@@ -130,9 +152,9 @@ int maxApples()
 
 		cout << endl << "The apple matrix after round " << k << " is: " << endl;
 
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < cols; j++)
 			{
 				cout<<apples[i][j]<<" ";
 			}
@@ -141,6 +163,16 @@ int maxApples()
 
 	}
 
+	//Delete dupApples and backtrackApples
+	
+	for (int i = 0; i < rows; i++)
+		delete[] dupApples[i];
+	delete[] dupApples;
+
+
+	for (int i = 0; i < rows; i++)
+		delete[] backtrackApples[i];
+	delete[] backtrackApples;
 
 	return totalApples;
 }
